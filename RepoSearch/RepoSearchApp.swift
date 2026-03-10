fdcbab2b2bd28dc3b2ac75d40029d61e557ee660
Sirius Kim <jmoumnet@gmail.com>
2026-03-10 14:06:00 +0900

    Initial Commit

---
+++ RepoSearch/RepoSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoSearchApp.swift
+//  RepoSearch
+//
+//  Created by JSS_MacBookPro on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct RepoSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
